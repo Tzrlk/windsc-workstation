d503f6a4aa512a442af7b6d64f0235ecf4b8ee79
Peter Cummuskey <peterc@aetheric.co.nz>
2023-11-30 15:55:17 +1300

    Adding to dsc build actions

--- project.build.ps1
+++ project.build.ps1
@@ -34,17 +34,78 @@ if ([System.IO.Path]::GetFileName($MyInvocation.ScriptName) -ne 'Invoke-Build.ps
     return
 }
 
+function Display-Status {
+
+    [CmdletBinding()]
+    param(
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNull()]
+        [Object]
+        $Status
+
+    )
+
+    Write-Output 'Resolved:'
+    if ( $Status.ResourcesInDesiredState ) {
+        $Status.ResourcesInDesiredState | ForEach-Object {
+            $LocationArgs = $_.SourceInfo.Replace("${PSScriptRoot}\", '').Replace('::', ' ').Split()
+            [PSCustomObject]@{
+                Name = $_.InstanceName
+                Type = $_.ResourceName
+                File = $LocationArgs[0]
+                Line = $LocationArgs[1]
+            }
+
+        } | Format-Table
+    }
+
+    Write-Output 'Unresolved:'
+    if ( $Status.ResourcesNotInDesiredState ) {
+        $Status.ResourcesNotInDesiredState | ForEach-Object {
+            $LocationArgs = $_.SourceInfo.Replace("${PSScriptRoot}\", '').Replace('::', ' ').Split()
+            [PSCustomObject]@{
+                Name   = $_.InstanceName
+                Type   = $_.ResourceName
+                File   = $LocationArgs[0]
+                Line   = $LocationArgs[1]
+
+            }
+        } | Format-Table
+    }
+
+}
+
 # TODO: Tasks for lint, pester, dsc.
 
 # https://learn.microsoft.com/en-us/powershell/dsc/getting-started/wingettingstarted?view=dsc-1.1
 
 task 'dsc-install' {
     Install-Module 'PSDscResources'
+	Install-Module 'xPSDesiredStateConfiguration'
 }
 
+task 'dsc-build' -Inputs .\configs\powershell.ps1 -Outputs .\Workstation\localhost.mof {
+ 	. .\configs\powershell.ps1
 
-task 'dsc-apply' {
+	Import-Module 'xPSDesiredStateConfiguration'
+	Configuration Workstation {
+ 		Node 'localhost' {
+   			Powershell
+		}
+ 	}
 
-    Start-DscConfiguration -Path '' -Wait -Verbose
+	Workstation
+}
+
+task 'dsc-test' -Inputs .\Workstation\localhost.mof {
+	$Result = Test-Dsc-Configuration -Path .\Workstation
+ 	Display-Status $Result
+}
+
+task 'dsc-apply' -Inputs .\Workstation\localhost.mof {
+
+    Start-DscConfiguration -Path .\Workstation -Force -Wait -Verbose
+	Display-Status ( Get-DscConfigurationStatus )
 
 }
