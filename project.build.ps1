f450a5e25cb8e6f5c0c154f0950ba693f1135f29
Peter Cummuskey <peterc@aetheric.co.nz>
2023-11-30 16:17:29 +1300

    updating build with linting and test actions.

--- project.build.ps1
+++ project.build.ps1
@@ -17,8 +17,7 @@
         > Invoke-Build task1, task2
         Tasks can also be invoked in the regular fashion.
 #>
-
-Param (
+param (
 
     [Parameter(Position=0)]
     [String[]] $Tasks
@@ -34,6 +33,7 @@ if ([System.IO.Path]::GetFileName($MyInvocation.ScriptName) -ne 'Invoke-Build.ps
     return
 }
 
+# TODO: Put this in its own file.
 function Display-Status {
 
     [CmdletBinding()]
@@ -76,19 +76,109 @@ function Display-Status {
 
 }
 
-# TODO: Tasks for lint, pester, dsc.
+
+Import-Module PSScriptAnalyzer
+Import-Module Pester
+
+$Sources = Get-ChildItem -Path . -Include *.ps1 -Exclude *.Tests.ps1 -Recurse -Name
+$Tests   = Get-ChildItem -Path . -Include *.Tests.ps1 -Recurse -Name
+$Modules = Get-ChildItem -Path . -Include *.psd1 -Recurse -Name
+
+# Synopsis: Statically analyses the codebase for badness.
+Add-BuildTask lint -Inputs ${Sources} -Outputs "${PSScriptRoot}/checkstyle.xml" {
+
+	# Run the linter, producing a list of result objects.
+	$Results = Invoke-ScriptAnalyzer `
+			-Path "${PSScriptRoot}" `
+			-CustomRulePath "${PSScriptRoot}/.lint-rules" `
+			-RecurseCustomRulePath `
+			-IncludeDefaultRules `
+			-ReportSummary `
+			-Recurse
+
+	# Get the total count of errors in the results.
+	$ErrorCount = ${Results} `
+		| Where-Object { ${_}.Severity -eq [DiagnosticSeverity]::Error } `
+		| Measure-Object
+
+	# Cannot have any errors at all
+	Assert-Build `
+		-Condition ( ${ErrorCount}.Count -eq 0 ) `
+		-Message "Cannot have any linting errors. $(${ErrorCount}.Count) errors found."
+
+	# Group results again by file to work better in the report.
+	$FileResults = ${Results} | Group-Object `
+		-Property ScriptName `
+		-AsHashTable
+
+	# Write a checkstyle-formatted report to disk.
+	# https://github.com/PowerShell/PSScriptAnalyzer/issues/1296
+	$XmlWriter = New-Object System.Xml.XmlTextWriter("${Outputs}", $Null)
+	try {
+		$XmlWriter.Formatting = 'Indented'
+		$XmlWriter.Indentation = 1
+		$XmlWriter.IndentChar = "`t"
+		$XmlWriter.WriteStartDocument()
+		$XmlWriter.WriteStartElement("checkstyle")
+		$XmlWriter.WriteAttributeString("version", "1.0.0")
+		foreach ($FileResult in (${FileResults} ?? @{}).GetEnumerator()) {
+			$XmlWriter.WriteStartElement("file")
+			$XmlWriter.WriteAttributeString("name", ${FileResult}.Name)
+			foreach ($Result in ${FileResult}.Value) {
+				$XmlWriter.WriteStartElement("error")
+				$XmlWriter.WriteAttributeString("line", ${Result}.Line)
+				$XmlWriter.WriteAttributeString("column", ${Result}.Column)
+				$XmlWriter.WriteAttributeString("severity", ${Result}.Severity)
+				$XmlWriter.WriteAttributeString("message", ${Result}.Message)
+				$XmlWriter.WriteAttributeString("source", ${Result}.RuleName)
+				$XmlWriter.WriteEndElement()
+			}
+			$XmlWriter.WriteEndElement()
+		}
+		$XmlWriter.WriteEndElement()
+		$XmlWriter.WriteEndDocument()
+		$XmlWriter.Flush()
+	} finally {
+		$XmlWriter.Close()
+	}
+
+}
+
+# Synopsis: Generates JUnit and JaCoCo output using availabel Pester tests.
+Add-BuildTask test -Inputs ( ${Sources} + ${Tests} ) -Outputs junit.xml, coverage.xml {
+	# https://pester.dev/docs/commands/New-PesterConfiguration
+	Invoke-Pester -Configuration @{
+		Run = @{
+			Path          = '.'
+			TestExtension = '.tests.ps1'
+			'Exit'        = $True
+		}
+		CodeCoverage = @{
+			Enabled      = $True
+			OutputFormat = 'JaCoCo'
+			OutputPath   = ${Outputs}[1]
+		}
+		TestResult = @{
+			Enabled      = $True
+			OutputFormat = 'JUnitXml'
+			OutputPath   = ${Outputs}[0]
+		}
+	}
+}
 
 # https://learn.microsoft.com/en-us/powershell/dsc/getting-started/wingettingstarted?view=dsc-1.1
 
 task 'dsc-install' {
-    Install-Module 'PSDscResources'
+	Install-Module 'PSDscResources'
 	Install-Module 'xPSDesiredStateConfiguration'
 }
 
 task 'dsc-build' -Inputs .\configs\powershell.ps1 -Outputs .\Workstation\localhost.mof {
- 	. .\configs\powershell.ps1
+ 	. .\configs\Powershell.ps1
 
 	Import-Module 'xPSDesiredStateConfiguration'
+
+ 	# TODO: This can probably be put in its own file.
 	Configuration Workstation {
  		Node 'localhost' {
    			Powershell
@@ -104,8 +194,9 @@ task 'dsc-test' -Inputs .\Workstation\localhost.mof {
 }
 
 task 'dsc-apply' -Inputs .\Workstation\localhost.mof {
-
-    Start-DscConfiguration -Path .\Workstation -Force -Wait -Verbose
+	Start-DscConfiguration -Path .\Workstation -Force -Wait -Verbose
 	Display-Status ( Get-DscConfigurationStatus )
-
 }
+
+# Synopsis: Default task.
+Add-BuildTask . lint, test, dsc-build, dsc-test
